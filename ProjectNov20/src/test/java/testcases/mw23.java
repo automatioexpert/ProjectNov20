5b0c76da842d42e91280eaafd972ccd1384bff33
Valmiki Vishwakarma <you@example.com>
2022-11-20 18:17:09 +0530

    add.

---
+++ ProjectNov20/src/test/java/testcases/mw23.java
@@ -0,0 +1,13 @@
+package testcases;
+
+public class mw23 {
+	{
+		System.out.println("bls");
+	}
+	public static void main(String[] args) {
+		System.out.println();
+		System.out.println("==");
+		new mw23();
+	}
+
+}
